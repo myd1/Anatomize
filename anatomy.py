4babcb4c5399127b174112145e1757cfa167f2bd
qiyuangong <qiyuangong@gmail.com>
2014-10-17 22:29:52 +0800

    Finish main functions of anatomy.
    Rewrite anatomy.
    
    Todo:
    read_data.py

--- anatomy.py
+++ anatomy.py
@@ -25,6 +25,7 @@ class SABucket(object):
 
     def __init__(self, data, index):
         self.member = data[:]
+        self.value = ""
         self.index = index
 
     def pop_element(self):
@@ -54,6 +55,15 @@ class Group(object):
         return False
 
 
+def list_to_str(value_list, sep=';'):
+    """covert sorted str list (sorted by default) to str 
+    value (splited by sep). This fuction is value safe, which means 
+    value_list will not be changed.
+    """
+    temp = value_list[:]
+    return sep.join(temp)
+
+
 def anatomy(data, L):
     """
     only one SA is supported in anatomy.
@@ -63,23 +73,32 @@ def anatomy(data, L):
     data is a list, i.e. [qi1,qi2,sa]
     """
     groups = []
+    buckets = {}
     result = []
     suppress = []
+    GT = []
+    ST = []
     h = []
     if _DEBUG:
         print '*' * 10
         print "Begin Anatomy!"
     print "L=%d" % L
-    # Group SA into buckets
-
-
-    for i, temp in enumerate(data):
+    # Assign SA into buckets
+    for temp in date:
+        list_temp = list_temp(temp[-1])
+        try:
+            buckets[list_temp].append(temp)
+        except:
+            buckets[list_temp] = [temp]
+    for i, temp in temp
+    # group stage
+    # use heap to sort buckets
+    for i, temp in enumerate(buckets.values()):
         # push to heap reversely
         pos = len(temp) * -1
         if pos == 0:
             continue
         heapq.heappush(h, (pos, SABucket(temp, i)))
-    # group step
     while len(h) >= L:
         newgroup = Group()
         length_list = []
@@ -98,7 +117,7 @@ def anatomy(data, L):
             # push new tuple to heap
             heapq.heappush(h, (length, temp))
         groups.append(newgroup)
-    # residue-assign step
+    # residue-assign stage
     while len(h):
         (length, temp) = heapq.heappop(h)
         index = temp.index
@@ -114,10 +133,18 @@ def anatomy(data, L):
     for i, t in enumerate(groups):
         t.index = i
         result.append(t.member[:])
-
+        for temp in t.member:
+            GT_temp = temp[:-1]
+            GT_temp.append(i)
+            SA_temp = temp[-1]
+            SA_temp.insert(0, i)
+            GT.append(GT_temp)
+            ST.append(SA_temp)
     if _DEBUG:
         print 'NO. of Suppress after anatomy = %d' % len(suppress)
         print 'NO. of groups = %d' % len(result)
+    for i in range(len(GT)):
+        print ST[i] + GT[i]
     return result
 
 
